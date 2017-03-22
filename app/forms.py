7a615f8af4996463afa8fda89d07f6b0bfbf8a40
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-03-21 21:12:18 -0400

    Finished CSS

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,9 @@ from wtforms import StringField, SelectMultipleField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, URL
 
 class ContactForm(Form):
-    first_name = StringField('first_name', validators=[DataRequired()])
-    last_name = StringField('last_name', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
     email_address = StringField('email', validators=[DataRequired(), Email()])
     website = StringField('website', validators=[URL()])
-    topics = SelectMultipleField('topics', choices=[("mech_eng","Mechanical Engineering")])
     request = TextAreaField('request', validators=[DataRequired()])
     submit = SubmitField('submit')
 
