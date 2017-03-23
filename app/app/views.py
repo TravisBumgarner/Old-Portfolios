0faa5b9daf1caf15d8ca80b59bbc0ce772811194
Travis Bumgarner <travis.bumgarner@gmail.com>
2017-03-23 17:32:50 -0400

    Updated html for email

--- app/app/views.py
+++ app/app/views.py
@@ -10,7 +10,17 @@ def index():
     contact_form = ContactForm()
     if contact_form.validate_on_submit():
         gmail = GMail(mail_from, mail_key)
-        gmail.send(Message('Message from {}'.format(contact_form.email_address.data), to= mail_to, text=contact_form.request.data))
+        msg = """
+                Name: {} <br>
+                Website: {} <br>
+                Email: {} <br>
+                Message: {}
+            """.format(contact_form.name.data,
+                       contact_form.website.data,
+                       contact_form.email_address.data,
+                       contact_form.message.data)
+
+        gmail.send(Message('Message from {}'.format(contact_form.email_address.data), to= mail_to, html=msg))
         flash('Thank you for your message. I will be in touch soon!')
         return redirect(url_for('index', _anchor="contact"))
     return render_template('index.html', contact_form=contact_form)
